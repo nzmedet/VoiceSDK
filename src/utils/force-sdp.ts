db737fbf09c18da00aba1b41aae8503113c6f73a
nzmedet <nzmedet@gmail.com>
2025-11-24 18:42:00 +1300

    revert sdp stereo for audio

--- src/utils/force-sdp.ts
+++ src/utils/force-sdp.ts
@@ -1,13 +1,12 @@
 export function forceSdpOptimization(sdp: string): string {
-  // Remove heavy features
+  // Remove heavy video/audio extensions
   sdp = sdp.replace(/a=rtcp-fb:111 transport-cc\r\n/g, '');
   sdp = sdp.replace(/a=extmap:\d+ .*transport-wide-cc.*\r\n/g, '');
   sdp = sdp.replace(/a=rtpmap:63 red\/48000\/2\r\n/g, '');
   sdp = sdp.replace(/a=fmtp:63 .*\r\n/g, '');
   sdp = sdp.replace(/a=extmap:\d+ .*abs-send-time.*\r\n/g, '');
 
-  // Force mono OPUS and cap bitrate
-  sdp = sdp.replace(/opus\/48000\/2/g, 'opus/48000/1');
+  // Keep stereo /2 in rtpmap, just disable stereo in fmtp
   sdp = sdp.replace(
     /a=fmtp:111 .*\r\n/,
     'a=fmtp:111 minptime=10;useinbandfec=1;stereo=0;maxaveragebitrate=24000\r\n'
